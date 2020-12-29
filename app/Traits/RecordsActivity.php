e652146c43807bd92876298cccaabe43c8bd3869
ilove3q <ilove3q@gmail.com>
2020-12-29 12:23:39 +0100

    fix error test

--- app/Traits/RecordsActivity.php
+++ app/Traits/RecordsActivity.php
@@ -12,7 +12,9 @@ trait RecordsActivity
     {
         if (auth()->guest()) return;
         foreach (static::getActivitiesToRecord() as $event) {
-            static::$event(fn ($model) => [$model->recordActivity($event)]);
+            static::$event(function ($model) use ($event) {
+                $model->recordActivity($event);
+            });
         }
 
         static::deleting(function ($model) {
